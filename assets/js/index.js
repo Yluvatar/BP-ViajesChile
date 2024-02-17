1f93e3a1a7c9465396dad1c623643dc578826ec0
Juan Pablo Scheihing <eru.yluvatar@gmail.com>
2024-02-17 00:29:02 -0300

    Se completa toda la estructura de la página y estilos, faltan algunos scripts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 
 $(function(){
-
-})
+    $("#frmContacto").on("submit", function (e) {
+        e.preventDefault();
+        alert("Mensaje enviado Sr/Sra " + $("#name").val() + "!");
+    });
+});
